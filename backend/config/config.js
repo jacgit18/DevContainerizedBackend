58b19e7c017a50b8b3b908e92be597acc05b8e1e
jacgit18 <joshuaxcarpentier@gmail.com>
2024-01-12 14:19:17 -0500

    little change

--- backend/config/config.js
+++ backend/config/config.js
@@ -32,7 +32,7 @@ const config = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: dbName,
+      database: process.env.TEST_DB_NAME, // put dbName back after
     },
   },
   forApiAlerts: 'forApiAlerts',
