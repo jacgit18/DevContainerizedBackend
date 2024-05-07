a8f5e525e34c748480e1be9f5fc116d428473417
jacgit18 <joshuaxcarpentier@gmail.com>
2024-05-07 13:12:39 -0400

    small change

--- backend/scripts/db_setup.sh
+++ backend/scripts/db_setup.sh
@@ -1,1 +1,13 @@
-sudo docker exec -it my_postgres_container psql -U postgres -c "DROP DATABASE proDB;"; sudo docker exec -it my_postgres_container psql -U postgres -c "CREATE DATABASE proDB;" && export FOR_TESTING=y && npx knex migrate:latest
+#!/bin/bash
+
+# Drop the existing database
+sudo docker exec my_postgres_container psql -U postgres -c "DROP DATABASE IF EXISTS postgres;"
+
+# Create a new database
+sudo docker exec my_postgres_container psql -U postgres -c "CREATE DATABASE postgres;"
+
+# Export environment variable
+export FOR_TESTING=y
+
+# Run migrations using knex
+npx knex migrate:latest
--- backend/scripts/test_db_setup.sh
+++ backend/scripts/test_db_setup.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+
 sudo docker exec -it my_postgres_container psql -U postgres -c "DROP DATABASE test;"; sudo docker exec -it my_postgres_container psql -U postgres -c "CREATE DATABASE test;" && export FOR_TESTING=y && npx knex migrate:latest
