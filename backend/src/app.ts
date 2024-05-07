234a743e0c069bdb935f90a36c1df40de5eff4eb
jacgit18 <joshuaxcarpentier@gmail.com>
2024-05-07 19:17:27 -0400

    commented out stuff temp

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,36 +2,44 @@
 import cors from "cors"
 import express from "express"
 import morgan from "morgan"
-
 import * as routes from "./routers/index.js"
 
+
 import config from "../config/config.js"
-import { initPerformanceMonitoring } from "../libs/monitoring.js"
-import { companyIdFromHeaders, convertQueryOperators } from "./middlewares/index.js"
+// import { initPerformanceMonitoring } from "../libs/monitoring.js"
+import { appUserIdFromHeaders, convertQueryOperators } from "./middlewares/index.js"
 // import { authenticateToken,  } from "./middlewares/index.js"
 
 
 
 const app = express()
 // NOTE: must be first
-app.use(initPerformanceMonitoring)
+// app.use(initPerformanceMonitoring)
 
 // Middle Wares
 if (!config.TESTING) {
   // dont need logging if we are in testing environment
-  app.use(morgan("tiny"))
+  console.log("Since ", !config.TESTING ," means were not in testing env ")
+  app.use(morgan("tiny")) // HTTP request logger
 }
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(convertQueryOperators)
-app.use(companyIdFromHeaders)
+// app.use(companyIdFromHeaders)
+app.use(appUserIdFromHeaders)
+
 
 
 // Unauthenticated user log in routes
-app.use( "/v1", routes.authRouter)
 // app.use( "/v1", routes.authRouter)
+
+// app.use( "/v1", routes.authRouter)
+
+
 app.use("/v1", routes.materialRouter)
+
+
 // app.use("/v1", authenticateToken, routes.materialRouter)
 
 
